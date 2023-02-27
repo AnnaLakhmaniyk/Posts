92eba0ff9d04719697178b5d1fdb84e50e85e148
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2023-02-27 16:45:57 +0200

    add header and footer

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+import Heading from "./Heading"
+export default function Footer() {
+  return (
+    <footer>
+      <Heading teg="h3" text="Created by webDev" />
+    </footer>
+  )
+}
---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import Heading from "./Heading"
+
+export default function Header() {
+  return (
+    <header>
+      <Heading teg="h3" text="Header" />
+    </header>
+  )
+}
